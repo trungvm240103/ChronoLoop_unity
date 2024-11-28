b658f63adb6ac3c367cb1a95f75cb404c0fac0f5
trungvm240103 <129076660+trungvm240103@users.noreply.github.com>
2024-11-28 10:05:35 +0700

    Initial commit

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5f; 
+    public float jumpForce = 10f; 
+
+    private Rigidbody2D rb;
+    private bool canControl = false; 
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (!canControl) return; 
+
+        
+        float move = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(move * moveSpeed, rb.velocity.y);
+
+        
+        if (Input.GetKeyDown(KeyCode.Space) && Mathf.Abs(rb.velocity.y) < 0.01f)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+    }
+
+    
+    public void SetControl(bool value)
+    {
+        canControl = value;
+    }
+
+  
+    public void Move(float direction)
+    {
+        rb.velocity = new Vector2(direction * moveSpeed, rb.velocity.y);
+    }
+
+    public void Jump()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+    }
+
+    public void JumpAndMove(float horizontalDirection)
+    {
+        rb.velocity = new Vector2(horizontalDirection * moveSpeed, jumpForce);
+    }
+
+}
